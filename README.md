3e5ee54b61d06d87a0ac1d59134a4c46c1d581e6
AtilaDev <atiladevelop@gmail.com>
2019-12-28 16:21:51 -0300

    Ver. 0.0.2 - awewinds with CRA working.

--- README.md
+++ README.md
@@ -4,14 +4,27 @@
 
 ### Configure your create-react-app or vue-cli project with Tailwind CSS quickly!
 
+##### On your root project run:
+
 ```
-$ npm install -g awewinds
+$ npx awewinds
 ```
 
-## Import your CSS in the index.js file
+## Instructions
+
+##### Open your package.json file, modify and add the next lines to "scripts" section:
+
+```
+"build:css":"tailwind build src/css/index.css -o src/css/tailwind.css",
+"start": "npm run build:css && react-scripts start",
+```
+
+##### Import your CSS in the index.js file
 
 ```
 import './css/tailwind.css';
 ```
 
-And that's all!
+##### And that's all!
+
+##### Please, if my tool help you, consider give a star! Thanks!
--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 #!/usr/bin/env node
 const figlet = require('figlet');
 const inquirer = require('inquirer');
-const pathBase = process.cwd();
-const fs = require('fs');
 const chalk = require('chalk');
+const fs = require('fs');
 const exec = require('child_process').exec;
-const { set } = require('edit-package-json');
 
-let package = require('./package.json');
-
-// Mostrar un banner con un mensaje formado por caracteres.
+// Show 'awewinds' banner
 const msn = msn => {
   console.log(
     chalk.bold.cyan(
@@ -22,7 +18,6 @@ const msn = msn => {
   );
 };
 
-// Preguntas que se van a realizar y que más tarde usaremos
 const queryParams = () => {
   const qs = [
     {
@@ -39,65 +34,49 @@ const queryParams = () => {
 function reactJS() {
   console.log(chalk.cyanBright('Installing Tailwind CSS in ReactJS...'));
 
-  // Creacion de carpetas y ficheros de configuracion
+  let postcssConfig = `const tailwindcss = require('tailwindcss');\nmodule.exports = {\n\tplugins: [\n\t\ttailwindcss('./tailwind.js'), require('autoprefixer')\n\t]\n};`;
 
-  // let str = JSON.stringify(package);
-  // let json = set(
-  //   str,
-  //   'scripts.start',
-  //   'tailwind build src/css/index.css -o src/css/tailwind.css && react-scripts start'
-  // );
+  fs.writeFile('./postcss.config.js', postcssConfig, function(err) {
+    if (err) {
+      return console.log(err);
+    }
+  });
 
-  // fs.writeFile('./package.json', json, function(err) {
-  //   if (err) {
-  //     return console.log(err);
-  //   }
-  // });
+  fs.mkdir('./src/css', function(err) {
+    if (err) {
+      return console.log(err);
+    }
 
-  // let postcssConfig = `const tailwindcss = require('tailwindcss');\nmodule.exports = {\n\tplugins: [\n\t\ttailwindcss('./tailwind.js'), require('autoprefixer')\n\t]\n};`;
+    let indexCSS = `@tailwind base;\n\n@tailwind components;\n\n@tailwind utilities;`;
 
-  // fs.writeFile('./postcss.config.js', postcssConfig, function(err) {
-  //   if (err) {
-  //     return console.log(err);
-  //   }
-  // });
+    fs.writeFile('./src/css/index.css', indexCSS, function(err) {
+      if (err) {
+        return console.log(err);
+      }
+    });
 
-  // fs.mkdir('./src/css', function(err) {
-  //   if (err) {
-  //     return console.log(err);
-  //   }
+    fs.writeFile('./src/css/tailwind.css', '', function(err) {
+      if (err) {
+        return console.log(err);
+      }
+    });
+  });
 
-  //   let indexCSS = `@tailwind base;\n\n@tailwind components;\n\n@tailwind utilities;`;
-
-  //   fs.writeFile('./src/css/index.css', indexCSS, function(err) {
-  //     if (err) {
-  //       return console.log(err);
-  //     }
-  //   });
-
-  //   fs.writeFile('./src/css/tailwind.css', '', function(err) {
-  //     if (err) {
-  //       return console.log(err);
-  //     }
-  //   });
-  // });
-
-  // Instalacion de librerias
-  // exec(
-  //   'npm install tailwindcss --dev && npm install postcss-cli autoprefixer --save-dev && npx tailwind init tailwind.js --full',
-  //   (err, stdout, stderr) => {
-  //     if (err) {
-  //       console.error(`exec error: ${err}`);
-  //       return;
-  //     }
-  //     console.log(`stdout: ${stdout}`);
-  //     console.log(chalk.cyanBright('Tailwind CSS installed in your project!'));
-  //   }
-  // );
+  // Libraries install
+  exec(
+    'npm install tailwindcss --dev && npm install postcss-cli autoprefixer --save-dev && npx tailwind init tailwind.js --full',
+    (err, stdout, stderr) => {
+      if (err) {
+        console.error(`exec error: ${err}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+      console.log(chalk.cyanBright('Tailwind CSS installed in your project!'));
+    }
+  );
 }
 
 function vueJS() {
-  // console.log(chalk.greenBright('Installing Tailwind CSS in VueJS...'));
   console.log(chalk.greenBright('Coming soon... ;)'));
 }
 
